dd80ec3c5f8984c80a19c73c8576f6c87f82a6db
Santiago31-hub <Santiago20462@gmail.com>
2025-03-18 12:13:38 -0300

    Implemente una función para actualizar la lista de amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -11,3 +11,12 @@ function agregarAmigo{
     actualizarlista();
     input.value = "";
 }
+function actualizarlista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
